6daaf5f9175bb4590a612ff3134402625c36acba
jairov4 <jairov4@gmail.com>
2014-12-22 06:48:25 +0000

    Diagram supports zoom

--- Northwood.UI/Diagram.cs
+++ Northwood.UI/Diagram.cs
@@ -11,7 +11,10 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
+using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace Northwood.UI
 {
@@ -22,9 +25,77 @@ namespace Northwood.UI
 			DefaultStyleKeyProperty.OverrideMetadata(typeof(Diagram), new FrameworkPropertyMetadata(typeof(Diagram)));
 		}
 
-		protected override bool IsItemItsOwnContainerOverride(object item) { return item is DiagramItem; }
+		protected override bool IsItemItsOwnContainerOverride(object item)
+		{
+			return item is DiagramItem;
+		}
 
-		protected override DependencyObject GetContainerForItemOverride() { return new DiagramItem(); }
+		protected override DependencyObject GetContainerForItemOverride()
+		{
+			return new DiagramItem();
+		}
+
+		public void NotifyItemClicked(DiagramItem diagramItem)
+		{
+			ToggleSelect(diagramItem);
+		}
+
+		private void ToggleSelect(DiagramItem diagramItem)
+		{
+			if (diagramItem.IsSelected)
+			{
+				Unselect(diagramItem);
+			}
+			else
+			{
+				Select(diagramItem);
+			}
+		}
+
+		private void Select(List<DiagramItem> items)
+		{
+			if (IsUpdatingSelectedItems) return;
+
+			BeginUpdateSelectedItems();
+			items.ForEach(a => SelectedItems.Add(ItemContainerGenerator.ItemFromContainer(a)));
+			EndUpdateSelectedItems();
+		}
+
+		private void Select(DiagramItem item)
+		{
+			Select(new List<DiagramItem>() { item });
+		}
+
+		private void Unselect(DiagramItem item)
+		{
+			if (IsUpdatingSelectedItems) return;
+
+			BeginUpdateSelectedItems();
+			SelectedItems.Remove(ItemContainerGenerator.ItemFromContainer(item));
+			EndUpdateSelectedItems();
+		}
+
+		public override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+			_content = Template.FindName("content", this) as FrameworkElement;
+			if (_content == null) return;
+
+			_content.MouseWheel += onMouseWheel;
+		}
+
+		void onMouseWheel(object sender, MouseWheelEventArgs e)
+		{
+			if (!Keyboard.IsKeyDown(Key.LeftCtrl) && !Keyboard.IsKeyDown(Key.RightCtrl)) return;
+
+			e.Handled = true;
+			_scaleFactor += e.Delta * 0.001;
+			_content.LayoutTransform = new ScaleTransform(_scaleFactor, _scaleFactor);
+		}
+
+		private double _scaleFactor = 1.0;
+		private FrameworkElement _content;
+
 	}
 
 	public class DiagramItem : ContentControl
@@ -38,6 +109,34 @@ namespace Northwood.UI
 		{
 			LeftItems = new ObservableCollection<object>();
 			RightItems = new ObservableCollection<object>();
+		}
+
+		public static readonly DependencyProperty IsSelectedProperty = Selector.IsSelectedProperty.AddOwner(typeof(DiagramItem), new FrameworkPropertyMetadata(false, OnIsSelectedChanged));
+
+		private static void OnIsSelectedChanged(DependencyObject target, DependencyPropertyChangedEventArgs e)
+		{
+			var item = (DiagramItem)target;
+
+			if ((bool)e.NewValue)
+			{
+				item.RaiseEvent(new RoutedEventArgs(Selector.SelectedEvent, item));
+			}
+			else
+			{
+				item.RaiseEvent(new RoutedEventArgs(Selector.UnselectedEvent, item));
+			}
+		}
+
+		public bool IsSelected
+		{
+			get
+			{
+				return (bool)GetValue(IsSelectedProperty);
+			}
+			set
+			{
+				SetValue(IsSelectedProperty, value);
+			}
 		}
 
 		public double X
@@ -133,6 +232,13 @@ namespace Northwood.UI
 		public static readonly DependencyProperty ItemTemplateSelectorProperty =
 			DependencyProperty.Register("ItemTemplateSelector", typeof(DataTemplateSelector), typeof(DiagramItem), new FrameworkPropertyMetadata(null));
 
+		protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
+		{
+			e.Handled = true;
+			var diagram = ItemsControl.ItemsControlFromItemContainer(this) as Diagram;
+			if (diagram != null)
+				diagram.NotifyItemClicked(this);
+		}
 	}
 
 	public class DiagramItemPort : HeaderedContentControl
